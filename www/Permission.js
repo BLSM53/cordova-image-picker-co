ab6500f58a5d2f0670cc4c9d9c2476ad04b6e5c6
yuqiusuo <yuqiusuo@qgutech.com>
2018-01-15 10:26:59 +0800

    picker save

--- www/Permission.js
+++ www/Permission.js
@@ -3,11 +3,11 @@ var Permission = function() {
 };
 
 Permission.prototype.hasReadPermission = function(callback) {
-    return cordova.exec(callback, null, "Permission", "hasPermission", []);
+    return cordova.exec(callback, null, "Permission", "hasReadPermission", []);
 };
 
 Permission.prototype.requestReadPermission = function(callback) {
-    return cordova.exec(callback, null, "Permission", "requestPermission", []);
+    return cordova.exec(callback, null, "Permission", "hasReadPermission", []);
 };
 
 window.Permission = new Permission();
