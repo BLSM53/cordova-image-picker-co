477d83064beba6ab9d59fab6f6cfd5608bf20578
yuqiusuo <yuqiusuo@qgutech.com>
2018-01-15 10:12:41 +0800

    picker save

--- src/android/Permission.java
+++ src/android/Permission.java
@@ -1,7 +1,10 @@
 package com.blsm.permission; 
 
-import org.apache.cordova.CallbackContext; 
-import org.apache.cordova.CordovaPlugin; 
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
