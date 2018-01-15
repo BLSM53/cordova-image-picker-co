533dca9a64177ebff2c2c01c61e0a07daa1aa50a
yuqiusuo <yuqiusuo@qgutech.com>
2018-01-15 09:40:39 +0800

    picker save

--- src/android/Permission.java
+++ src/android/Permission.java
@@ -2,6 +2,9 @@ package com.synconset;
 
 import org.apache.cordova.CallbackContext; 
 import org.apache.cordova.CordovaPlugin; 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import org.apache.cordova.PluginResult; 
 
